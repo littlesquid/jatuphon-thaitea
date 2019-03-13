80249210fadec7e90f7e0bfd13e7fe22d82044c0
littlesquid <sirikarn.r@scale360solutions.com>
2019-03-13 16:50:38 +0700

    Updates

---
+++ precache-manifest.4728f1386f53b81671cd174f69b4c99c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/jatuphon-thaitea/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d592fd92fb9705ee206f",
+    "url": "/jatuphon-thaitea/static/js/runtime~main.e00313ca.js"
+  },
+  {
+    "revision": "21ced045822bbe2d30ab",
+    "url": "/jatuphon-thaitea/static/js/main.ad655375.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/jatuphon-thaitea/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "21ced045822bbe2d30ab",
+    "url": "/jatuphon-thaitea/static/css/main.a3766a47.chunk.css"
+  },
+  {
+    "revision": "64aa3e343ec4cf3ce305dd70a6cfdaa7",
+    "url": "/jatuphon-thaitea/index.html"
+  }
+];
